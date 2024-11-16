22c4de3cdeeb6db33b2b838781baead72742e743
장주현 <joohyun852@gmail.com>
2024-11-17 04:33:10 +0900

    init commit

---
+++ Kiwoom_trading_system_joohyun_2nd/KiwoomStart.bat
@@ -0,0 +1,10 @@
+@ECHO ON
+
+title Kiwoom Start
+
+cd C:\Kiwoom_trading\Kiwoom_trading_system_joohyun_2nd
+
+call C:\Users\coden\anaconda3\Scripts\activate.bat trading_hyun
+python __init__.py
+
+cmd.exe
